c104a7c2d319419d905a9d6f73f939c2277f3e9a
Nekonekoo <patrickfetscher60@gmail.com>
2019-05-08 15:50:31 +0200

    Updated for 81.03

---
+++ index.js
@@ -0,0 +1,75 @@
+const DEFAULT_HOOK_SETTINGS = {order: -1000000, filter: {fake: false}};
+const JOB_VALK = 12;
+
+const SKILL_RUNEBURST = 161200;
+const SKILL_RUNEBURST_R = 166200;
+
+
+module.exports = function ValkFastRB(mod) {
+	const {command} = mod;
+	
+	let gameId,
+		model,
+		job,
+		enabled = false;
+
+	let CANCEL_DELAY = 600;
+
+	command.add('valkrb', {
+		$default() { 
+			enabled = !enabled;
+        	command.message(`Valkyrie fast runeburst is now ${enabled ? "enabled" : "disabled"}.`);
+    	},
+    	delay(value) {
+    		value = parseInt(value);
+    		if (!isNaN(value)) {
+    			CANCEL_DELAY = value;
+    			command.message(`Runeburst delay cancel set to ${CANCEL_DELAY}.`);
+    		}
+    	}
+	});
+
+	mod.hook('S_LOGIN', 13, DEFAULT_HOOK_SETTINGS, event => {
+	    gameId = event.gameId;
+	    model = event.templateId;
+	    job = (model - 10101) % 100;
+	    enabled = [JOB_VALK].includes(job);
+	});
+
+	mod.hook('S_ACTION_STAGE', 9, {order: -1000000, filter: {fake: null}}, event => {
+		if (!enabled || event.gameId !== gameId) return;
+
+	    if (event.skill.id == SKILL_RUNEBURST || event.skill.id == SKILL_RUNEBURST + 30) {
+	    	event.skill.id = SKILL_RUNEBURST
+	    } else if (event.skill.id == SKILL_RUNEBURST_R || event.skill.id == SKILL_RUNEBURST_R + 30) {
+	    	event.skill.id = SKILL_RUNEBURST_R;
+	    }
+
+	    if ([SKILL_RUNEBURST, SKILL_RUNEBURST_R].includes(event.skill.id)) {
+	    	mod.setTimeout(() => {
+	    		mod.toClient('S_ACTION_END', 5, {
+					gameId,
+					loc: { x: event.loc.x, y: event.loc.y, z: event.loc.z },
+					w: event.w,
+					templateId: model,
+					skill: event.skill.id,
+					type: 12394123,
+					id: event.id,
+				});
+	    	}, CANCEL_DELAY);
+	    }
+	});
+
+	mod.hook('S_ACTION_END', 5, {order: -1000000, filter: {fake: null}}, event => {
+	    if (!enabled || event.gameId !== gameId) return;
+
+	   	if (event.skill.id == SKILL_RUNEBURST || event.skill.id == SKILL_RUNEBURST + 30 || event.skill.id == SKILL_RUNEBURST_R || event.skill.id == SKILL_RUNEBURST_R + 30) {
+	   		if (event.type == 12394123) {
+	   			event.type = 4;
+	   			return true;
+	   		} else {
+	   			return false;
+	   		}
+	   	}
+	});
+}
